69d7e26f33dad62ba7915c03fa39b008de36f9eb
lukehayes <lukepurplehayes@gmail.com>
2021-10-11 10:02:53 +0100

    Added hasMany association to Board model.

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Board extends Model
 {
     use HasFactory;
+
+    public function tasks()
+    {
+        return $this->HasMany(Task::class);
+    }
 }
