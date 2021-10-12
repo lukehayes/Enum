3a9ae73f9891d6d1fb891b8a0130d098622cac45
lukehayes <lukepurplehayes@gmail.com>
2021-10-12 12:00:17 +0100

    Added TextComponent.vue to project.

---
+++ resources/js/components/TextComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>A lovely text component</p>
+</template>
+
+/*----------------------------------------*/
+<script>
+
+export default
+{
+    mounted()
+    {
+        console.log("Text Component Mounted");
+    }
+}
+
+</script>
